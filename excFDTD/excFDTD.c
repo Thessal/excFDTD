ec269784539177283e61b2caae399cf1f2544c93
Jongkook Choi - Laptop <jongkook90@gmail.com>
2016-07-14 16:01:36 +0900

    fixed strange bug in Dielectric update kernel

--- excFDTD/excFDTD.c
+++ excFDTD/excFDTD.c
@@ -8,9 +8,12 @@
 #define _DimZ (30)
 
 #define _S_factor (2.0f)
-#define _c0 (3e8)
-#define _dx (1e-6)
+//#define _c0 (3e8)
+//#define _dx (1e-6)
+#define _c0 (1.0f)
+#define _dx (1.0f)
 #define _dt (_dx / _c0 / _S_factor)
+#define _cdt (_dx / _S_factor)
 float stability_factor_inv = 1.0f / _S_factor; //not good
 
 // doi:10.1088/0022-3727/40/22/043, Au
@@ -32,29 +35,31 @@ float stability_factor_inv = 1.0f / _S_factor; //not good
 #define _a12_div_eps0 (-2.0f * _Delta_eps2_L * _Omega2_L * sin(_phi2_L))
 #define _b01 (_Omega1_L * _Omega1_L + _Gamma1_L * _Gamma1_L)
 #define _b02 (_Omega2_L * _Omega2_L + _Gamma2_L * _Gamma2_L)
-#define _b11 (2 * _Gamma1_L)
-#define _b12 (2 * _Gamma2_L)
+#define _b11 (2.0f * _Gamma1_L)
+#define _b12 (2.0f * _Gamma2_L)
 #define _b21 (1.0f)
 #define _b22 (1.0f)
 
-#define _C1_ ( (_b21 / _dt / _dt) + (_b11 / 2 / _dt) + (_b01 / 4) )
-#define _C2_ ( (_b22 / _dt / _dt) + (_b12 / 2 / _dt) + (_b02 / 4) )
-#define _C11 ( ( ( 2*_b21/_dt/_dt ) - (_b01*0.5f) ) / _C1_ )
-#define _C12 ( ( ( 2*_b22/_dt/_dt ) - (_b02*0.5f) ) / _C2_ )
-#define _C21 ( ( ( _b11 * 0.5f / _dt ) - ( _b21 / _dt / _dt ) - ( _b01 / 4 ) ) / _C1_ )
-#define _C22 ( ( ( _b12 * 0.5f / _dt ) - ( _b22 / _dt / _dt ) - ( _b02 / 4 ) ) / _C2_ )
-#define _C31 ( ( ( _a01 / 4.0f ) + ( _a11 / 2 / _dt ) ) / _C1_ )
-#define _C32 ( ( ( _a02 / 4.0f ) + ( _a12 / 2 / _dt ) ) / _C2_ )
+#define _C1_ ( (_b21 / _dt / _dt) + (_b11 * 0.5f / _dt) + (_b01 * 0.25f) )
+#define _C2_ ( (_b22 / _dt / _dt) + (_b12 * 0.5f / _dt) + (_b02 * 0.25f) )
+#define _C11 ( ( ( 2.0f*_b21/_dt/_dt ) - (_b01*0.5f) ) / _C1_ )
+#define _C12 ( ( ( 2.0f*_b22/_dt/_dt ) - (_b02*0.5f) ) / _C2_ )
+#define _C21 ( ( ( _b11 * 0.5f / _dt ) - ( _b21 / _dt / _dt ) - ( _b01 * 0.25f ) ) / _C1_ )
+#define _C22 ( ( ( _b12 * 0.5f / _dt ) - ( _b22 / _dt / _dt ) - ( _b02 * 0.25f ) ) / _C2_ )
+#define _C31 ( ( ( _a01 * 0.25f ) + ( _a11 * 0.5f / _dt ) ) / _C1_ )
+#define _C32 ( ( ( _a02 * 0.25f ) + ( _a12 * 0.5f / _dt ) ) / _C2_ )
 #define _C41 ( _a01 * 0.5f / _C1_ )
 #define _C42 ( _a02 * 0.5f / _C2_ )
-#define _C51 ( ( _a01 / 4.0f ) - ( _a11 * 0.5f / _dt ) ) / _C1_
-#define _C52 ( ( _a02 / 4.0f ) - ( _a12 * 0.5f / _dt ) ) / _C2_
+#define _C51 ( ( _a01 * 0.25f ) - ( _a11 * 0.5f / _dt ) ) / _C1_
+#define _C52 ( ( _a02 * 0.25f ) - ( _a12 * 0.5f / _dt ) ) / _C2_
 
-//#define _d1_
-//#define _d11
-//#define _d21
-//#define _d31
-//#define _d41
+//single drude pole
+#define _dq_ ( ( 1.0f / _dt / _dt ) + ( _gamma_D  * 0.5f / _dt ) )
+#define _d1q ( 2.0f / _dq_ / _dt / _dt )
+#define _d2q ( ( _gamma_D * 0.5f / _dt ) - ( 1 / _dt / _dt ) ) / _dq_
+#define _d3q_div_eps0 ( _omega_D * _omega_D * 0.25f / _dq_ )
+#define _d4q_div_eps0 ( _omega_D * _omega_D * 0.5f / _dq_ )
+#define _d5q_div_eps0 _d3q_div_eps0
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -70,7 +75,7 @@ float stability_factor_inv = 1.0f / _S_factor; //not good
 #define _sizeofFloat (32)
 
 //3D loop tiling with 1px padding, for L1 cache or cuda SHMEM utilization
-//do we need 1px padding?
+//1px padding required for easy L1 cache optimization
 #define _gridDimX ((_DimX + (_blockDimX-2) - 1) / (_blockDimX-2))
 #define _gridDimY ((_DimY + (_blockDimY-2) - 1) / (_blockDimY-2))
 #define _gridDimZ ((_DimZ + (_blockDimZ-2) - 1) / (_blockDimZ-2))
@@ -114,11 +119,10 @@ __m256* mHz = (__m256*)Hz;
 __m256* meps = (__m256*)eps_r_inv;
 
 int init(void);
-void DielectricE(void);
-void DielectricH(void);
+void Dielectric_HE(void);
+void Dielectric_HE_C(void);
+void DCP_HE_C(void);
 void syncPadding(void);
-void DrudeE(void);
-void DrudeH(void);
 int snapshot(void);
 int main(int argc, char* argv[])
 {
@@ -141,20 +145,75 @@ int main(int argc, char* argv[])
 	printf("b11     \t%01.2e\n", _b11);
 	printf("b21     \t%01.2e\n", _b21);
 
-//	init();
-/*
-	DielectricH();
-	DielectricE();
+	init();
+	
 	syncPadding();
-*/
-
-//	snapshot();
+	Dielectric_HE(); //FIXME : asm update E strange result
+	//Dielectric_HE_C();
+	syncPadding();
+	
+	snapshot();
 	return 0;
 }
 
-void DielectricE(void)
+void Dielectric_HE_C(void)
+{
+	for (unsigned __int64 offset = 0; offset < _threadPerGrid; offset += 1) {
+		Hx[offset] -= (eps0_c_Ey[offset] - eps0_c_Ey[offset + _offsetZ] + eps0_c_Ez[offset + _offsetX] - eps0_c_Ez[offset + _offsetX]) * _cdt;
+		Hy[offset] -= (eps0_c_Ez[offset] - eps0_c_Ez[offset + _offsetX] + eps0_c_Ex[offset + _offsetZ] - eps0_c_Ex[offset]) *_cdt;
+		Hz[offset] -= (eps0_c_Ex[offset + _offsetZ] - eps0_c_Ex[offset + _offsetY + _offsetZ] + eps0_c_Ey[offset + _offsetZ] - eps0_c_Ey[offset - _offsetX + _offsetZ]) * _cdt;
+		eps0_c_Ex[offset] += (Hy[offset - _offsetZ] - Hy[offset] + Hz[offset - _offsetZ] - Hz[offset - _offsetY - _offsetZ]) * eps_r_inv[offset] * _cdt;
+		eps0_c_Ey[offset] += (Hz[offset - _offsetZ] - Hz[offset + _offsetX - _offsetZ] + Hx[offset] - Hx[offset - _offsetZ]) * eps_r_inv[offset] * _cdt;
+		eps0_c_Ez[offset] += (Hx[offset - _offsetX - _offsetY] - Hx[offset - _offsetX] + Hy[offset] - Hy[offset - _offsetX]) * eps_r_inv[offset] * _cdt;
+	}
+}
+
+void Dielectric_HE(void)
 {
 	for (unsigned __int64 offset = 0; offset < _threadPerGrid / 8; offset += 1) {
+		_mm256_store_ps(Hx + offset * 8, // 1 * ymm(32byte) = 8 * float(4byte)
+			_mm256_sub_ps(
+				mHx[offset],
+				_mm256_mul_ps(
+					_mm256_sub_ps(
+						_mm256_add_ps(
+							_mm256_sub_ps(
+								mEy[offset],
+								mEy[offset + _offsetZ / 8]),
+							_mm256_loadu_ps(eps0_c_Ez + offset + _offsetX + _offsetY)),
+						_mm256_loadu_ps(eps0_c_Ez + offset + _offsetX)),
+					_mm256_broadcast_ss(&stability_factor_inv))
+			)
+		);
+		_mm256_store_ps(Hy + offset * 8, // 1 * ymm(32byte) = 8 * float(4byte)
+			_mm256_sub_ps(
+				mHy[offset],
+				_mm256_mul_ps(
+					_mm256_sub_ps(
+						_mm256_add_ps(
+							_mm256_sub_ps(
+								mEz[offset],
+								_mm256_loadu_ps(eps0_c_Ez + offset + _offsetX)),
+							mEx[offset + _offsetZ / 8]),
+						mEx[offset]),
+					_mm256_broadcast_ss(&stability_factor_inv))
+			)
+		);
+		_mm256_store_ps(Hz + offset * 8, // 1 * ymm(32byte) = 8 * float(4byte)
+			_mm256_sub_ps(
+				mHz[offset],
+				_mm256_mul_ps(
+					_mm256_sub_ps(
+						_mm256_add_ps(
+							_mm256_sub_ps(
+								mEx[offset + _offsetZ / 8],
+								mEx[offset + _offsetY / 8 + _offsetZ / 8]),
+							mEy[offset + _offsetZ / 8]),
+						_mm256_loadu_ps(eps0_c_Ey + offset - _offsetX + _offsetZ)),
+					_mm256_broadcast_ss(&stability_factor_inv))
+			)
+		);
+
 		_mm256_store_ps(eps0_c_Ex + offset * 8, // 1 * ymm(32byte) = 8 * float(4byte)
 			_mm256_add_ps(
 				_mm256_mul_ps(
@@ -162,7 +221,7 @@ void DielectricE(void)
 						_mm256_sub_ps(
 							_mm256_add_ps(
 								_mm256_sub_ps(
-									mHy[offset - _offsetZ/8],
+									mHy[offset - _offsetZ/8], //strange
 									mHy[offset]),
 								mHz[offset - _offsetZ / 8]),
 							mHz[offset - _offsetY / 8 - _offsetZ / 8]),
@@ -203,72 +262,32 @@ void DielectricE(void)
 	}
 }
 
-void DielectricH(void) 
+void DCP_HE_C(void)
 {
-	for (unsigned __int64 offset = 0; offset < _threadPerGrid / 8; offset += 1) {
-		_mm256_store_ps(Hx + offset * 8, // 1 * ymm(32byte) = 8 * float(4byte)
-			_mm256_sub_ps(
-				mHx[offset],
-				_mm256_mul_ps(
-						_mm256_sub_ps(
-							_mm256_add_ps(
-								_mm256_sub_ps(
-									mEy[offset],
-									mEy[offset + _offsetZ/8]),
-								_mm256_loadu_ps(eps0_c_Ez + offset + _offsetX + _offsetY)),
-							_mm256_loadu_ps(eps0_c_Ez + offset + _offsetX)),
-					_mm256_broadcast_ss(&stability_factor_inv))
-			)
-		);
-		_mm256_store_ps(Hy + offset * 8, // 1 * ymm(32byte) = 8 * float(4byte)
-			_mm256_sub_ps(
-				mHy[offset],
-				_mm256_mul_ps(
-					_mm256_sub_ps(
-						_mm256_add_ps(
-							_mm256_sub_ps(
-								mEz[offset],
-								_mm256_loadu_ps(eps0_c_Ez + offset + _offsetX) ),
-							mEx[offset + _offsetZ ]),
-						mEx[offset]),
-					_mm256_broadcast_ss(&stability_factor_inv))
-			)
-		);
-		_mm256_store_ps(Hz + offset * 8, // 1 * ymm(32byte) = 8 * float(4byte)
-			_mm256_sub_ps(
-				mHz[offset],
-				_mm256_mul_ps(
-					_mm256_sub_ps(
-						_mm256_add_ps(
-							_mm256_sub_ps(
-								mEx[offset + _offsetZ / 8],
-								mEx[offset + _offsetY / 8 + _offsetZ / 8]),
-							mEy[offset + _offsetZ / 8] ) ,
-						_mm256_loadu_ps(eps0_c_Ey + offset - _offsetX + _offsetZ)),
-					_mm256_broadcast_ss(&stability_factor_inv))
-			)
-		);
+	for (unsigned __int64 offset = 0; offset < _threadPerGrid; offset += 1) {
+		Hx[offset] -= (eps0_c_Ey[offset] - eps0_c_Ey[offset + _offsetZ] + eps0_c_Ez[offset + _offsetX] - eps0_c_Ez[offset + _offsetX]) * _cdt;
+		Hy[offset] -= (eps0_c_Ez[offset] - eps0_c_Ez[offset + _offsetX] + eps0_c_Ex[offset + _offsetZ] - eps0_c_Ex[offset]) * _cdt;
+		Hz[offset] -= (eps0_c_Ex[offset + _offsetZ] - eps0_c_Ex[offset + _offsetY + _offsetZ] + eps0_c_Ey[offset + _offsetZ] - eps0_c_Ey[offset - _offsetX + _offsetZ]) * _cdt;
+		eps0_c_Ex[offset] += (Hy[offset - _offsetZ] - Hy[offset] + Hz[offset - _offsetZ] - Hz[offset - _offsetY - _offsetZ]) * eps_r_inv[offset] * _cdt;
+		eps0_c_Ey[offset] += (Hz[offset - _offsetZ] - Hz[offset + _offsetX - _offsetZ] + Hx[offset] - Hx[offset - _offsetZ]) * eps_r_inv[offset] * _cdt;
+		eps0_c_Ez[offset] += (Hx[offset - _offsetX - _offsetY] - Hx[offset - _offsetX] + Hy[offset] - Hy[offset - _offsetX]) * eps_r_inv[offset] * _cdt;
 	}
 }
 
-void DCP_E(void)
-{
-
-}
-
-#define _syncX(FIELD) \
+#define _syncX_(FIELD) \
 FIELD[_INDEX_THREAD(X - 1, Y, Z, _blockDimX - 1, yy, zz)] = FIELD[_INDEX_THREAD(X, Y, Z, 1, yy, zz)]; \
-FIELD[_INDEX_THREAD(X, Y, Z, 0, yy, zz)] = FIELD[_INDEX_THREAD(X - 1, Y, Z, _blockDimX - 2, yy, zz)];
-#define _syncY(FIELD) \
+FIELD[_INDEX_THREAD(X, Y, Z, 0, yy, zz)] = FIELD[_INDEX_THREAD(X - 1, Y, Z, _blockDimX - 2, yy, zz)]; 
+#define _syncY_(FIELD) \
 FIELD[_INDEX_THREAD(X, Y - 1, Z, xx, _blockDimY - 1, zz)] = FIELD[_INDEX_THREAD(X, Y, Z, xx, 1, zz)]; \
-FIELD[_INDEX_THREAD(X, Y, Z, xx, 0, zz)] = FIELD[_INDEX_THREAD(X, Y - 1, Z, xx, _blockDimY - 2, zz)];
-#define _syncZ(FIELD) \
+FIELD[_INDEX_THREAD(X, Y, Z, xx, 0, zz)] = FIELD[_INDEX_THREAD(X, Y - 1, Z, xx, _blockDimY - 2, zz)]; 
+#define _syncZ_(FIELD) \
 FIELD[_INDEX_THREAD(X, Y, Z - 1, xx, yy, _blockDimZ - 1)] = FIELD[_INDEX_THREAD(X, Y, Z, xx, yy, 1)]; \
 FIELD[_INDEX_THREAD(X, Y, Z, xx, yy, 0)] = FIELD[_INDEX_THREAD(X, Y, Z - 1, xx, yy, _blockDimZ - 2)];
-#define _syncXall _syncX(eps0_c_Ex) _syncX(eps0_c_Ey) _syncX(eps0_c_Ez)  _syncX(Hx)  _syncX(Hy) _syncX(Hz) 
-#define _syncYall _syncY(eps0_c_Ex) _syncY(eps0_c_Ey) _syncY(eps0_c_Ez)  _syncY(Hx)  _syncY(Hy) _syncY(Hz) 
-#define _syncZall _syncZ(eps0_c_Ex) _syncZ(eps0_c_Ey) _syncZ(eps0_c_Ez)  _syncZ(Hx)  _syncZ(Hy) _syncZ(Hz) 
+#define _syncXall _syncX_(eps0_c_Ex) _syncX_(eps0_c_Ey) _syncX_(eps0_c_Ez)  _syncX_(Hx)  _syncX_(Hy) _syncX_(Hz) 
+#define _syncYall _syncY_(eps0_c_Ex) _syncY_(eps0_c_Ey) _syncY_(eps0_c_Ez)  _syncY_(Hx)  _syncY_(Hy) _syncY_(Hz) 
+#define _syncZall _syncZ_(eps0_c_Ex) _syncZ_(eps0_c_Ey) _syncZ_(eps0_c_Ez)  _syncZ_(Hx)  _syncZ_(Hy) _syncZ_(Hz) 
 
+//이거 안넣으면 boundary에서 index 1 shift 있음
 //#define _syncBoundaryX(FIELD) \
 //FIELD[_INDEX_THREAD(_gridDimX-1, Y, Z, _blockDimX - 1, yy, zz)] = FIELD[_INDEX_THREAD(0, Y, Z, 1, yy, zz)]; \
 //FIELD[_INDEX_THREAD(0, Y, Z, 0, yy, zz)] = FIELD[_INDEX_THREAD(_gridDimX-1, Y, Z, _blockDimX - 2, yy, zz)]; \
@@ -303,8 +322,13 @@ int init(void)
 		Y += (tmp % _gridDimY) * (_blockDimY-2); tmp /= _gridDimY;
 		Z += (tmp % _gridDimZ) * (_blockDimZ-2); tmp /= _gridDimZ;
 
-//		Hy[i] = (float)(X * 10000 + Y * 100 + Z);
-//		Hz[i] = (float)(X * 10000 + Y * 100 + Z);
+		eps0_c_Ey[i] = (float)(0.0f);
+		eps0_c_Ez[i] = (float)(0.0f);
+		Hx[i] = (float)(0.0f);
+		Hy[i] = (float)(0.0f);
+		Hz[i] = (float)(0.0f);
+		//Hy[i] = (float)(X * 10000 + Y * 100 + Z);
+		//Hz[i] = (float)(X * 10000 + Y * 100 + Z);
 		eps_r_inv[i] = 1.0f;
 
 
@@ -318,20 +342,27 @@ int init(void)
 	}
 	return 0;
 }
+#define TEMP eps0_c_Ex
+//#define TEMP Hy
 int snapshot(void)
 {
 	printf("Z=5\n");
+	int Z = 5;
 	for (int Y = 0; Y < _DimY; Y++) {
+		//if (Y%_blockDimY == 0) { for (int X = 0; X < _DimX; X++) { printf("%07.3f ", eps0_c_Ex[_INDEX_XYZ(X, -1, Z)]);  } 	printf("\n");}
 		for (int X = 0; X < _DimX; X++) {
-			int Z = 5;
-			printf("%02.1f \t", eps0_c_Ex[_INDEX_XYZ(X, Y, Z) ]);
+			//if (X%_blockDimX == 0) { printf("%07.1f ", eps0_c_Ex[_INDEX_XYZ(-1, Y, Z)]);}
+			printf("%07.3f ", TEMP[_INDEX_XYZ(X, Y, Z) ]);
 			//printf("%06.0f \t", eps0_c_Ex[_INDEX_XYZ(X, Y, Z)]);
 			//printf("%d \t", _INDEX_BLOCK(X/(_blockDimX-2), Y/(_blockDimY-2), Z/(_blockDimZ-2)));
 			//printf("%d \t",Y/_blockDimY);
 			//printf("%d \t",_INDEX_XYZ(X,Y,Z));
+			//if (X%_blockDimX == _blockDimX-1) { printf("%07.3f ", eps0_c_Ex[_INDEX_XYZ(_blockDimX, Y, Z)]); }
 		}
 		printf("\n");
+		//if (Y%_blockDimY == _blockDimY -1 ){			for (int X = 0; X < _DimX; X++) 	{				printf("%07.3f ", eps0_c_Ex[_INDEX_XYZ(X, _blockDimY, Z)]);			}printf("\n");		}
 	}
+	return;
 	printf("\n");
 	printf("Y=2\n");
 	for (int Z = 0; Z < _DimZ; Z++) {
